792ed86b5fabeb07467c0548922f0e39cf442616
leandro <leandro@pop-os.localdomain>
2024-10-29 17:43:01 -0300

    feat: finish first timeline with gsap

--- script.js
+++ script.js
@@ -25,13 +25,31 @@ tl.to("#bottle-berry", {
     width: "20%",
 }, "one")
 
+tl.to("#strawberry", {
+    top: "183%",
+    left: "26.5%",
+    width: "5%",
+}, "one")
 
 
+tl.to("#mangoslice", {
+    top: "185%",
+    left: "60%",
+    width: "5%",
+}, "one")
 
+tl.to("#berry", {
+    top: "184%",
+    left: "-8.5%",
+    width: "5%",
+}, "one")
 
-
-
-
+tl.to("#berry2", {
+    top: "120%",
+    left: "105%",
+    width: "15%",
+    rotate: "120deg",
+}, "one")
 
 
 
